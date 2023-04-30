2770fcf546d3cc0ca510f3ebd51edbdfa342c5a3
Sol Lee <sadlthf@icloud.com>
2023-04-30 14:28:30 +0900

    Time: 288 ms (55.83%), Space: 31.9 MB (11.30%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         
         my_dict = dict()
-        st_dict = dict()
         
         if len(nums) == 0 :
             return 0
@@ -14,23 +13,16 @@ class Solution(object):
         for i in nums:
             if i not in my_dict: # 중복되는 숫자 없도록
                 my_dict[i] = i
-        
-        # [0, 2, 4, 5, 6, 7, -1, -8, -4, -3, -2]
-        # [0, -1, -2, -3, -4]
-        # [-3, -4, -1, -2, 0]
-        # [4, 5, 6, 7]
             
+        max_len = 1
         for i in my_dict:
-            if i + 1 in my_dict and i -1 not in my_dict:
-                st_dict[i] = i
-                
-        res0 = 1
-        for i in st_dict:
             res = 1
-            while i+1 in my_dict:
-                res = res + 1
-                i = i + 1
-            if res0 < res:
-                res0 = res
+            if i + 1 in my_dict and i -1 not in my_dict: # 연속되는 숫자의 첫번째 숫자만 저장
+                res = 1
+                while i + 1 in my_dict:
+                    res = res + 1
+                    i = i + 1
+            if max_len < res:
+                max_len = res
                 
-        return res0
+        return max_len
