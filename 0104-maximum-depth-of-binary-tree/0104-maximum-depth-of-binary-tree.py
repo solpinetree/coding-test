8e8f129c0d4c2f7aeacab1f76c09e727c9ee402e
Sol Lee <sadlthf@icloud.com>
2023-06-19 16:50:30 +0900

    Time: 28 ms (57.65%), Space: 16 MB (55.54%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -11,26 +11,24 @@ class Solution(object):
         :rtype: int
         """
         # bfs - level order 문제
+        max_depth = 0
         if root is None:
-            return 0
+            return max_depth
         
         q = deque()
         q.append((root,1))
-        res = 1
         
         while q:
-            cur_node_level = q.popleft()
-            cur_node = cur_node_level[0]
-            cur_level = cur_node_level[1]
+            cur_node, cur_depth = q.popleft()
+
             if cur_node.left:
-                q.append((cur_node.left, cur_level+1))
+                q.append((cur_node.left, cur_depth+1))
             if cur_node.right:
-                q.append((cur_node.right, cur_level+1))
-            
-            if res < cur_level: 
-                res = cur_level
-            
-        return res
+                q.append((cur_node.right, cur_depth+1))
+                
+            max_depth = max(cur_depth, max_depth)
+          
+        return max_depth
         
         
         
