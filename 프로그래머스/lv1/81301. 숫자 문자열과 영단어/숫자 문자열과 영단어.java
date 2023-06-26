1b16404bd082a111d51c47c95c90819b6b282fc4
Sol Lee <sadlthf@icloud.com>
2023-06-26 16:39:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,44 +1,16 @@
-import java.util.HashMap;
+// import java.util.HashMap;
 
 class Solution {
-    public int solution(String s) {
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put("zero", "0");
-        map.put("one", "1");
-        map.put("two", "2");
-        map.put("three", "3");
-        map.put("four", "4");
-        map.put("five", "5");
-        map.put("six", "6");
-        map.put("seven", "7");
-        map.put("eight", "8");
-        map.put("nine", "9");
-
-        char[] s_arr = s.toCharArray();
-        String res = "";
-        
-        for(int i = 0; i < s.length(); i++) {
-            if(Character.isDigit(s_arr[i])){
-                res = res + s_arr[i];
-                continue;
-            }
-            
-            String temp = "";
-            while(i<s.length() && !Character.isDigit(s_arr[i])){
-                temp = temp + s_arr[i];
-                i++;
-                
-                if(map.containsKey(temp)){
-                    break;
-                }
-            }
-            
-            res = res + map.get(temp);
-            i--;
+    private static final String[] words ={ 
+        "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine"
+    };
+    
+    public int solution(String s) {       
+        for(int i = 0; i < words.length; i++) {
+            s = s.replace(words[i], Integer.toString(i));
         }
         
-        System.out.println(res);
-        
-        return Integer.parseInt(res);
+        return Integer.parseInt(s);
     }
 }
