cea73e4260ef78b75414dd74773857b263b46e34
Sol Lee <sadlthf@icloud.com>
2024-09-25 14:36:36 +0900

    Update 성격 유형 검사하기.py

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,30 +1,19 @@
 def solution(survey, choices):
     answer = ''
     
-    # choices : [i번째 캐릭터, 점수]
-    scores = {
-        1: [0, 3],
-        2: [0, 2],
-        3: [0, 1],
-        4: [0, 0],
-        5: [1, 1],
-        6: [1, 2],
-        7: [1, 3]
-    }
+    score_mapping = {"RT":0, "CF":0, "JM":0, "AN":0}
     
-    types = "RTCFJMAN"
-    types_score = {type: 0 for type in types}
-    
-    for i in range(len(survey)):
-        sur_type = survey[i] 
-        choice = choices[i]
-        
-        types_score[sur_type[scores[choice][0]]] += scores[choice][1] 
-        
-    for i in range(0, len(types) - 1, 2):
-        if types_score[types[i]] < types_score[types[i+1]]:
-            answer += types[i+1]
+    for type, choice in zip(survey, choices):
+        if type not in score_mapping.keys():
+            type = type[::-1]
+            score_mapping[type] += choice - 4
         else:
-            answer += types[i]
+            score_mapping[type] -= choice - 4
     
+    for type in score_mapping.keys():
+        if score_mapping[type] < 0:
+            answer += type[1]
+        else:
+            answer += type[0]
+            
     return answer
