a84cdfaab9acdb971b2852918bb9c88aa8548885
Sol Lee <sadlthf@icloud.com>
2024-10-06 14:01:01 +0900

    Update 다리를 지나는 트럭.py

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,32 +1,27 @@
-# 모든 트럭이 다리를 건너려면 최소 몇 초 걸리는지
-# bridge_length : 트럭이 최대 몇대 올라갈 수 있는지
-# weight : 다리는 무게를 얼마나 견딜 수 있는지
-# FIFO
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    answer = bridge_length
-    
-    bridge = deque()
-    cur_weight = 0
-    idx = 0
-            
-    while idx < len(truck_weights):
-        if cur_weight + truck_weights[idx] <= weight:
-            bridge.append(truck_weights[idx])
-            cur_weight += truck_weights[idx]
-            idx += 1
-        else:
-            bridge.append(0)
+    # 현재 다리를 건너고 있는 트럭들을 나타내는 큐 (트럭 무게 + 다리 위에 있는 시간)
+    bridge = deque([0] * bridge_length)
+    cur_weight = 0  # 현재 다리 위에 있는 트럭의 총 무게
+    time = 0  # 경과 시간
 
-        if len(bridge) == bridge_length:
+    for truck in truck_weights:
+        while True:
+            # 1초 경과, 다리 위에 있는 가장 앞 트럭이 다리를 빠져나옴
+            time += 1
             cur_weight -= bridge.popleft()
-            
-        answer += 1
-        
-    # while bridge and max(bridge) != 0:
-    #     print(bridge)
-    #     answer += 1
-    #     bridge.popleft()
 
-    return answer
+            # 현재 트럭이 다리 위에 오를 수 있는지 확인
+            if cur_weight + truck <= weight:
+                bridge.append(truck)
+                cur_weight += truck
+                break
+            else:
+                # 무게를 초과하면 트럭이 다리에 오르지 않고 다리에서 0만 추가
+                bridge.append(0)
+
+    # 마지막 트럭이 다리를 완전히 건너기까지의 시간 추가
+    time += bridge_length
+
+    return time
