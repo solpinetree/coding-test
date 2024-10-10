61765a1d6e5437317ebb9dfb78cd5da894a3ca9b
Sol Lee <sadlthf@icloud.com>
2024-10-10 15:30:47 +0900

    Time: 17 ms (24.54%), Space: 11.5 MB (85.21%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,44 +1,40 @@
 class MyQueue(object):
 
     def __init__(self):
-        self.stack = []
+        self.input_stack = []  # 입력 스택
+        self.output_stack = []  # 출력 스택
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
-        # [1]
-        # [2, 1]
-        # []
-        self.stack = [x] + self.stack
-        
+        self.input_stack.append(x)
 
     def pop(self):
         """
         :rtype: int
         """
-        return self.stack.pop()
-        
+        self._move_input_to_output()
+        return self.output_stack.pop()
 
     def peek(self):
         """
         :rtype: int
         """
-        return self.stack[-1]
-        
+        self._move_input_to_output()
+        return self.output_stack[-1]
 
     def empty(self):
         """
         :rtype: bool
         """
-        return not self.stack
-        
+        return not self.input_stack and not self.output_stack
 
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+    def _move_input_to_output(self):
+        """
+        입력 스택의 요소를 출력 스택으로 옮김으로써 큐의 순서를 유지
+        """
+        if not self.output_stack:  # 출력 스택이 비어 있을 때만 이동
+            while self.input_stack:
+                self.output_stack.append(self.input_stack.pop())
