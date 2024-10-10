526edf8ee2413d454f31aeef895d44951a58ec6e
Sol Lee <sadlthf@icloud.com>
2024-10-10 15:31:02 +0900

    Time: 16 ms (37.99%), Space: 11.4 MB (97.73%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,40 +1,41 @@
 class MyQueue(object):
 
     def __init__(self):
-        self.input_stack = []  # 입력 스택
-        self.output_stack = []  # 출력 스택
+        self.stack = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
-        self.input_stack.append(x)
+        self.stack = [x] + self.stack
+        
 
     def pop(self):
         """
         :rtype: int
         """
-        self._move_input_to_output()
-        return self.output_stack.pop()
+        return self.stack.pop()
+        
 
     def peek(self):
         """
         :rtype: int
         """
-        self._move_input_to_output()
-        return self.output_stack[-1]
+        return self.stack[-1]
+        
 
     def empty(self):
         """
         :rtype: bool
         """
-        return not self.input_stack and not self.output_stack
+        return not self.stack
+        
 
-    def _move_input_to_output(self):
-        """
-        입력 스택의 요소를 출력 스택으로 옮김으로써 큐의 순서를 유지
-        """
-        if not self.output_stack:  # 출력 스택이 비어 있을 때만 이동
-            while self.input_stack:
-                self.output_stack.append(self.input_stack.pop())
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
