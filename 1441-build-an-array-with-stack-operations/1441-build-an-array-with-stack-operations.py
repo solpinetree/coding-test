555db6fe8a6efe51ac6b933835ae7b28f3a380a5
Sol Lee <sadlthf@icloud.com>
2024-10-10 14:33:38 +0900

    Time: 9 ms (92.81%), Space: 11.4 MB (87.58%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -6,16 +6,14 @@ class Solution(object):
         :rtype: List[str]
         """
         stack = []
-        target_idx = 0
+        target_set = set(target) # Set 을 사용하여 검색을 O(1)로 수행
         
         for i in range(1, n+1):
-            if target_idx < len(target):
-                if target[target_idx] == i:
-                    stack.append('Push')
-                    target_idx += 1
-                else:
-                    stack.append('Push')
-                    stack.append('Pop')
+            if i > target[-1]: # target의 마지막 숫자를 넘어서면 종료
+                break
+            stack.append('Push')
+            if i not in target_set:
+                stack.append('Pop') # target에 없는 경우에만 Pop 추가
                 
         return stack
         
