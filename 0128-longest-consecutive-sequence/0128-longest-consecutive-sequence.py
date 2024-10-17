bdc29579017cf3dfb7c525ced37e914c971dede7
Sol Lee <sadlthf@icloud.com>
2024-10-17 23:08:38 +0900

    Time: 44 ms (100.00%), Space: 26.2 MB (89.72%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -7,31 +7,21 @@ class Solution(object):
         :rtype: int
         """
         
-        dict = {}
+        num_set = set(nums)
+        max_length = 0
         
-        for num in nums:
-            if num in dict:
-                continue
+        for num in num_set:
+            # num - 1이 num_set에 없을 경우만 연속된 시퀀스의 시작점으로 간수
+            if num - 1 not in num_set: 
+                current_num = num
+                current_streak = 1
                 
-            dict[num] = [num, num]
-            if num - 1 in dict and num + 1 in dict:
-                s = dict[num-1][0]
-                m = dict[num+1][1]
-                dict[s][1] = m
-                dict[m][0] = s
-            elif num -1 in dict:
-                s = dict[num-1][0]
-                dict[s][1] = num
-                dict[num][0] = s
-            elif num + 1 in dict:
-                m = dict[num+1][1]
-                dict[m][0] = num
-                dict[num][1] = m
-        
-        answer = 0
-        
-        for key in dict:
-            answer = max(answer, dict[key][1] - dict[key][0] + 1)
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+                    
+                # 가장 긴 시퀀스를 저장
+                max_length = max(max_length, current_streak)
             
-        return answer
+        return max_length
         
