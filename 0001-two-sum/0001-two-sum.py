202866433bfee79c3e7600447208593cf353acc9
Sol Lee <sadlthf@icloud.com>
2024-10-17 22:15:11 +0900

    Time: 3 ms (100.00%), Space: 13.2 MB (14.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,3 +1,6 @@
+# return indices of the two numbers such that they add up to target
+from collections import defaultdict
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -5,9 +8,15 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        my_dict = dict()
-        for i, j in enumerate(nums):
-            if target - j in my_dict:
-                return [my_dict[target-j], i]
-            my_dict[j] = i
+        dict = defaultdict(list)
+        
+        for i, n in enumerate(nums):
+            dict[n].append(i)
             
+        
+        for key1 in dict:
+            key2 = target - key1
+            if key1 == key2 and len(dict[key1]) > 1:
+                return [dict[key1][0], dict[key1][1]]
+            if key2 in dict and dict[key2] != dict[key1]:
+                return [dict[key1][0], dict[key2][0]]
