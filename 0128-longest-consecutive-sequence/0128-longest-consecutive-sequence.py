feed5cfb98f3d13e30c0f7572434440fe7767343
Sol Lee <sadlthf@icloud.com>
2024-10-17 23:03:23 +0900

    Time: 187 ms (100.00%), Space: 35.3 MB (5.04%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,3 +1,5 @@
+# nums : unsorted array of integers
+# return the len of the longest consecutive elements sequence
 class Solution(object):
     def longestConsecutive(self, nums):
         """
@@ -5,15 +7,31 @@ class Solution(object):
         :rtype: int
         """
         
-        num_set = set(nums)
-        longest = 0
+        dict = {}
         
-        for i in num_set:
-            cnt = 1
-            if i + 1 in num_set and i -1 not in num_set: # 연속되는 숫자의 첫번째 숫자만 저장
-                while i + 1 in num_set:
-                    cnt += 1
-                    i += 1
-            longest = max(longest, cnt)
+        for num in nums:
+            if num in dict:
+                continue
                 
-        return longest
+            dict[num] = [num, num]
+            if num - 1 in dict and num + 1 in dict:
+                s = dict[num-1][0]
+                m = dict[num+1][1]
+                dict[s][1] = m
+                dict[m][0] = s
+            elif num -1 in dict:
+                s = dict[num-1][0]
+                dict[s][1] = num
+                dict[num][0] = s
+            elif num + 1 in dict:
+                m = dict[num+1][1]
+                dict[m][0] = num
+                dict[num][1] = m
+        
+        answer = 0
+        
+        for key in dict:
+            answer = max(answer, dict[key][1] - dict[key][0] + 1)
+            
+        return answer
+        
