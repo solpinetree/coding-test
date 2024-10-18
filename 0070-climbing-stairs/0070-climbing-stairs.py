187618e164b0e4e6ad8c90e0807cea70a3b71128
Sol Lee <sadlthf@icloud.com>
2024-10-18 20:44:58 +0900

    Time: 0 ms (100.00%), Space: 11.5 MB (61.68%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,7 +1,8 @@
+from collections import deque
 class Solution(object):
     # it takes n steps to reach the top
     # each time you can either climb 1 or 2 steps
-    # in how many distinct ways can you climb to the top?
+    # return how many distinct ways u can climb to the top
     def climbStairs(self, n):
         """
         :type n: int
@@ -13,12 +14,13 @@ class Solution(object):
         if n == 2:
             return 2
         
-        prev2 = 1
-        prev1 = 2
+        q = deque([(1, 1), (2, 2)])
+        
+        for i in range(3, n+1):
+            stairs, ways = q.popleft()
+            q.append((i, ways + q[0][1]))
             
-        for i in range(3, n + 1):
-            current = prev2 + prev1
-            prev2 = prev1
-            prev1 = current
+        while q:
+            answer = q.popleft()[1]
             
-        return prev1
+        return answer
