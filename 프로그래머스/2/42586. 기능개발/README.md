3296122ef1d1ef067da8901b9fda4c66c394d9e0
Sol Lee <sadlthf@icloud.com>
2024-10-21 15:50:02 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.10 ms
+메모리: 10.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 06일 19:10:10
+2024년 10월 21일 15:50:00
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,27 +1,26 @@
-# 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses
-# 각 작업의 개발 속도가 적힌 정수 배열 speeds
-# 각 배포마다 몇 개의 기능이 배포되는지
-from collections import deque
+# progresses : 먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열
+# speeds : 각 작업의 개발 속도가 적힌 정수 배열
+# return : 각 배포마다 몇 개의 기능이 배포되는지
+# 배포는 하루에 한 번만. 하루의 끝에 이루어짐.
 import math 
 
 def solution(progresses, speeds):
+    days = []
     answer = []
-    q = deque()
-    a = 1
-    tmp = 0
     
-    for i, p in enumerate(progresses):
-        days = math.ceil((100 - p)/speeds[i])
-        if not q:
-            q.append(days)
-            a = 1
+    for p, s in zip(progresses, speeds):
+        days.append(math.ceil((100-p)/s))
+    
+    deploy = days[0]
+    deploy_n = 0
+    
+    for day in days:
+        if day <= deploy:
+            deploy_n += 1
         else:
-            tmp = max(tmp, q.popleft())
-            if tmp >= days:
-                a += 1
-            else:
-                answer.append(a)
-                a = 1
-            q.append(days)
-    answer.append(a)
+            answer.append(deploy_n)
+            deploy_n = 1
+            deploy = day
+    
+    answer.append(deploy_n)
     return answer
