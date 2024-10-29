1dadf819599f7078ab90744fca8e375bf494d4e7
Sol Lee <sadlthf@icloud.com>
2024-10-29 13:06:13 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.34 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
